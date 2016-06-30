edc0d6027927fbae825b0d6abead8f04d9c74848
Carlos Camacho <carloscamachoucv@hotmail.com>
2016-06-30 11:51:49 +0200

    Update push.py

--- scripts/push.py
+++ scripts/push.py
@@ -11,19 +11,19 @@ import sys
 def getGit(*args):
   return subprocess.check_call(['git'] + list(args))
 
-def getNits(es,folder):
-  for nit in glob.glob(folder):
-    with open(nit, 'r') as stream:
+def getKb(es,folder):
+  for item in glob.glob(folder):
+    with open(item, 'r') as stream:
       try:
         doc=yaml.load(stream)
-        doc['name']=nit.split('/')[2]
+        doc['name']=item.split('/')[2]
         doc["err"]= "<br />".join(doc["err"].split("\n"))
         doc["sol"]= "<br />".join(doc["sol"].split("\n"))
-        pushNit(es,doc)
+        pushKb(es,doc)
       except yaml.YAMLError as exc:
         print(exc)
 
-def pushNit(es,doc):
+def pushKb(es,doc):
   url=es+doc.get("name")
   headers = {'content-type': 'application/json'}
   response = requests.post(url, data=json.dumps(doc), headers=headers)
@@ -34,13 +34,14 @@ def main(args):
     es=str(sys.argv[1])
   else:
     print "ES server not provided."
-    print "Usage: push.py <ES URL>/<index>/<type>/"
+    print "Usage: push.py <ES URL>/<index>"
     exit()
 
-  shutil.rmtree("tripleo-nit-db","true")
-  getGit("clone", "git://github.com/ccamacho/tripleo-nit-db.git", "-b", "master")
-  getNits(es,"tripleo-nit-db/nits/*.nit")
-  shutil.rmtree("tripleo-nit-db","true")
+  shutil.rmtree("tripleo-kb","true")
+  getGit("clone", "git://github.com/ccamacho/tripleo-kb.git", "-b", "master")
+  getKb(es+"/nits/","tripleo-kb/nits/*.nit")
+  getKb(es+"/tips/","tripleo-kb/tips/*.tip")
+  shutil.rmtree("tripleo-kb","true")
 
 if __name__ == '__main__':
     main(sys.argv)
